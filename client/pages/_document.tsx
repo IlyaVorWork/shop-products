7542445767fcc7a58e86fc6da518c19a47097c59
IlyaVorWork <77672212+IlyaVorWork@users.noreply.github.com>
2021-01-29 20:32:20 +0700

    initial commit

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -18,6 +18,7 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
+          <title>Main</title>
         </Head>
         <body>
           <Main />
