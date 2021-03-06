e317f5d69712da61cc5679e02b391089e8673810
IlyaVorWork <77672212+IlyaVorWork@users.noreply.github.com>
2021-03-06 14:59:00 +0700

    Delete client directory

--- client/components/ui/IconButton/IconButton.styles.tsx
+++
@@ -1,9 +0,0 @@
-import { createStyles, makeStyles } from '@material-ui/core/styles'
-
-export const useStyles = makeStyles(() =>
-  createStyles({
-    iconButton: {
-      borderRadius: 0,
-    },
-  })
-)
--- client/components/ui/TabPanel/TabPanel.styles.tsx
+++
@@ -1,9 +0,0 @@
-import { createStyles, makeStyles } from '@material-ui/core/styles'
-
-export const useStyles = makeStyles(() =>
-  createStyles({
-    panel: {
-      width: '100%',
-    },
-  })
-)
